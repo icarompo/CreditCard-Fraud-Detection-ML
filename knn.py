3bdde6a8402f554743ffb471f2f331fc92a42748
icarompo <icaromesquitaponce@outlook.com>
2023-09-19 21:15:01 -0300

    report removed from individual files

--- knn.py
+++ knn.py
@@ -16,14 +16,6 @@ knn_model.fit(X_train, y_train)
 
 y_pred = knn_model.predict(X_test)
 
-accuracy = accuracy_score(y_test, y_pred)
-confusion = confusion_matrix(y_test, y_pred)
-classification_rep = classification_report(y_test, y_pred)
-
-print("\nConfusion Matrix:\n", confusion)
-print("\nClassification Report:\n", classification_rep)
-print("Model accuracy:", accuracy)
-
 plt.figure(figsize=(8, 6))
 plt.scatter(X_test['distance_from_home'], X_test['distance_from_last_transaction'], c=y_pred, cmap='coolwarm', marker='o', edgecolor='k')
 plt.title('KNN Results')
