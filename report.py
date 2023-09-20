90678d9ae3bfa18125016b2cb8ce97337d9691cc
icarompo <icaromesquitaponce@outlook.com>
2023-09-19 21:04:35 -0300

    corrections and report

---
+++ report.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+dataset = pd.read_csv('card_transdata.csv')
+
+X = dataset.drop('fraud', axis=1)
+y = dataset['fraud']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+naive_bayes_model = GaussianNB()
+naive_bayes_model.fit(X_train, y_train)
+y_pred_nb = naive_bayes_model.predict(X_test)
+
+decision_tree_model = DecisionTreeClassifier(random_state=42)
+decision_tree_model.fit(X_train, y_train)
+y_pred_tree = decision_tree_model.predict(X_test)
+
+knn_model = KNeighborsClassifier(n_neighbors=5)
+knn_model.fit(X_train, y_train)
+y_pred_knn = knn_model.predict(X_test)
+
+accuracy_nb = accuracy_score(y_test, y_pred_nb)
+confusion_nb = confusion_matrix(y_test, y_pred_nb)
+classification_rep_nb = classification_report(y_test, y_pred_nb)
+
+accuracy_tree = accuracy_score(y_test, y_pred_tree)
+confusion_tree = confusion_matrix(y_test, y_pred_tree)
+classification_rep_tree = classification_report(y_test, y_pred_tree)
+
+accuracy_knn = accuracy_score(y_test, y_pred_knn)
+confusion_knn = confusion_matrix(y_test, y_pred_knn)
+classification_rep_knn = classification_report(y_test, y_pred_knn)
+
+def print_confusion_matrix_with_labels(confusion_matrix):
+    tp, fp, fn, tn = confusion_matrix.ravel()
+    print("\nMatriz de Confusão:")
+    print(f"\tVerdadeiros Positivos (TP): {tp}")
+    print(f"\tFalsos Positivos (FP): {fp}")
+    print(f"\tFalsos Negativos (FN): {fn}")
+    print(f"\tVerdadeiros Negativos (TN): {tn}")
+
+print("Resultados para Naive Bayes:")
+print_confusion_matrix_with_labels(confusion_nb)
+print("\nRelatório de Classificação:\n", classification_rep_nb)
+print("\nAcurácia do modelo:", accuracy_nb)
+
+print("\n\nResultados para Árvore de Decisão:")
+print_confusion_matrix_with_labels(confusion_tree)
+print("\nRelatório de Classificação:\n", classification_rep_tree)
+print("\nAcurácia do modelo:", accuracy_tree)
+
+print("\n\nResultados para KNN:")
+print_confusion_matrix_with_labels(confusion_knn)
+print("\nRelatório de Classificação:\n", classification_rep_knn)
+print("\nAcurácia do modelo:", accuracy_knn)
--- tree.py
+++ tree.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 dataset = pd.read_csv('card_transdata.csv')
@@ -24,7 +24,11 @@ print("\nConfusion Matrix:\n", confusion)
 print("\nClassification Report:\n", classification_rep)
 print("Model accuracy:", accuracy)
 
-plt.figure(figsize=(12, 8))
-plot_tree(decision_tree_model, filled=True, feature_names=X.columns.tolist(), class_names=['Non Fraud', 'Fraud'], fontsize=8)
-plt.title('Decision Tree Results')
+plt.figure(figsize=(8, 6))
+plt.scatter(X_test['distance_from_home'], X_test['distance_from_last_transaction'], c=y_pred, cmap='coolwarm', marker='o', edgecolor='k')
+plt.title('Decision Tree Results - Scatter Plot')
+plt.xlabel('Distance from home')
+plt.ylabel('Distance from last transaction')
+
+plt.colorbar(label='Classification (0 - Non Fraud, 1 - Fraud)')
 plt.show()
